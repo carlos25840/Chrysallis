3a76332485a184b6ac0f112ee5d066901b9876d6
Juan Leon <carlos25840@hotmail.com>
2020-01-31 13:18:27 +0100

    Colores  mj me obliga

--- app/src/main/java/com/example/chrysallis/MainActivity.java
+++ app/src/main/java/com/example/chrysallis/MainActivity.java
@@ -3,22 +3,19 @@ package com.example.chrysallis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.WindowManager;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*Controla que la activity se ponga en modo pantalla completa*/
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
+    /*
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.my_menu, menu);
         return true;
-    }
+    }*/
 }
