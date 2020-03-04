c6ab02c6653ef725f848134fb6728f3a7d3366fb
María José Benites Gaitán <mariajosebg95@gmail.com>
2020-03-04 12:27:58 +0100

    Api

--- app/src/main/java/com/example/chrysallis/Api/ApiService/SociosService.java
+++ app/src/main/java/com/example/chrysallis/Api/ApiService/SociosService.java
@@ -1,10 +1,14 @@
 package com.example.chrysallis.Api.ApiService;
 
+import com.example.chrysallis.classes.Socio;
+
 import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface SociosService {
+    @GET("api/Socios")
+    Call<ArrayList<Socio>> getSocios();
 
 }
