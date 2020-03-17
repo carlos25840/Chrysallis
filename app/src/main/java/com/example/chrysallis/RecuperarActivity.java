be378bc51663c775ef28b031a84c6a7938d54fa4
Juan Leon <carlos25840@hotmail.com>
2020-03-17 18:20:19 +0100

    cambiar clave

--- app/src/main/java/com/example/chrysallis/RecuperarActivity.java
+++ app/src/main/java/com/example/chrysallis/RecuperarActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,18 @@ import android.widget.Toast;
 import com.example.chrysallis.Api.Api;
 import com.example.chrysallis.Api.ApiService.SociosService;
 import com.example.chrysallis.classes.Socio;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -42,7 +55,7 @@ public class RecuperarActivity extends AppCompatActivity {
                                 Socio socio = response.body();
                                 if(socio != null){
                                     enviarMail();
-                                    Toast.makeText(getApplicationContext(),"Su contraseña ha sido enviada", Toast.LENGTH_LONG).show();
+                                    //Toast.makeText(getApplicationContext(),"Su contraseña ha sido enviada", Toast.LENGTH_LONG).show();
                                 }else{
                                     Toast.makeText(getApplicationContext(),R.string.loginIncorrect, Toast.LENGTH_LONG).show();
                                 }
@@ -64,7 +77,54 @@ public class RecuperarActivity extends AppCompatActivity {
     }
 
     public void enviarMail(){
-        new SendMailTask(RecuperarActivity.this).execute("carlos25840@gmail.com",
-                "jcleon20055", "mariajosebg95@gmail.com ", "prueba", "Tu nueva clave es: 052hdst");
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+        Mail m=new Mail("eventschrysallis@gmail.com","chrysallis2005");
+
+        String[] toArr = {"carlos25840@gmail.com", "mariajosebg95@gmail.com"};
+        m.setTo(toArr);
+        m.setFrom("Chrysallis");
+        m.setSubject("Chrysallis password");
+
+
+
+        m.setBody("Tu nueva clave de Crysallis Events es: " + getPassword(8));
+
+        try {
+            boolean i= m.send();
+            if(i==true){
+                Toast.makeText(getApplicationContext(),"Email was sent successfully ",Toast.LENGTH_LONG).show();
+
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"Email was not sent successfully ",Toast.LENGTH_LONG).show();
+
+            }
+
+        } catch (Exception e2) {
+            Toast.makeText(getApplicationContext(), e2.toString(), Toast.LENGTH_LONG).show();
+
+        }
+
     }
+
+    public String getPassword(int length){
+        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Random rand = new Random();
+
+        for(int i = 0; i < length; i++){
+            char c = chars[rand.nextInt(chars.length)];
+            stringBuilder.append(c);
+        }
+
+        return stringBuilder.toString();
+    }
+
+
+
 }
