938c11080a40c734cee0d522524cf1f7e1d22891
María José Benites Gaitán <mariajosebg95@gmail.com>
2020-03-23 13:41:06 +0100

    Arreglo para que funcione la dirección.

--- app/src/main/java/com/example/chrysallis/EventoActivity.java
+++ app/src/main/java/com/example/chrysallis/EventoActivity.java
@@ -1,29 +1,25 @@
 package com.example.chrysallis;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.Toolbar;
 
 import com.example.chrysallis.classes.Evento;
+import com.example.chrysallis.components.GeocodingLocation;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.io.IOException;
-import java.util.List;
 
 public class EventoActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
@@ -53,7 +49,6 @@ public class EventoActivity extends AppCompatActivity implements OnMapReadyCallb
         txtTime.setText(time);
         txtDescription.setText(evento.getDescripcion());
         txtLocation.setText(evento.getUbicacion());
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -75,24 +70,9 @@ public class EventoActivity extends AppCompatActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         String address = evento.getUbicacion();
-        Geocoder geocoder = new Geocoder(this);
-        List<Address> addresses = null;
-        try {
-            addresses = geocoder.getFromLocationName(address, 1);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if(addresses != null) {
-            double latitude= addresses.get(0).getLatitude();
-            double longitude= addresses.get(0).getLongitude();
-            // Add a marker in Sydney and move the camera
-            LatLng place = new LatLng(latitude, longitude);
-            mMap.addMarker(new MarkerOptions().position(place).title(getString(R.string.event)));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
-        }else{
-            LatLng place = new LatLng(41.384724, 2.171768);
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
-        }
+        GeocodingLocation locationAddress = new GeocodingLocation();
+        locationAddress.getAddressFromLocation(address,
+                getApplicationContext(), new GeocoderHandler());
     }
 
     @Override
@@ -104,4 +84,34 @@ public class EventoActivity extends AppCompatActivity implements OnMapReadyCallb
         }
         return true;
     }
+
+    private class GeocoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String locationAddress;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    locationAddress = bundle.getString("address");
+                    Log.d("latttt",locationAddress);
+                    break;
+                default:
+                    locationAddress = null;
+            }
+            if(locationAddress != null){
+                String[] result = locationAddress.split(";");
+                double latitude= Double.parseDouble(result[0]);
+                double longitude= Double.parseDouble(result[1]);
+                // Add a marker in Sydney and move the camera
+                LatLng place = new LatLng(latitude,longitude);
+                mMap.addMarker(new MarkerOptions().position(place).title(getString(R.string.event)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
+            }else{
+                LatLng place = new LatLng(41.384724, 2.171768);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(place));
+            }
+        }
+    }
+
+
 }
